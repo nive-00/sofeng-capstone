135c5cf31e6f3c0f80bf5cab18639338ca30493e
nive-00 <130848418+nive-00@users.noreply.github.com>
2023-06-26 03:53:22 +0800

    templating

--- announcements/views.py
+++ announcements/views.py
@@ -1,9 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-# Create your views here.
+posts = [
+    {
+        'author':'niv',
+        'title': 'post1',
+        'content': 'first content',
+        'date_posted': 'june 6,2023',
+    },{
+        'author':'jomar',
+        'title': 'post2',
+        'content': 'second content',
+        'date_posted': 'june 8,2023',
+    }
+]
 def home(request):
-    return HttpResponse('<h1>Announcements</h1>')
+    context = {
+        'posts' : posts
+    }
+    return render(request, 'announcements/home.html',context)
 
 def about(request):
-    return HttpResponse('<h1>About</h1>')
+    return render(request, 'announcements/about.html', {'title':'hello'})
